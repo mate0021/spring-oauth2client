29383d3876428b66ba24da886967003cdbfe21ff
mate0021 <mami.ext@gmail.com>
2021-11-01 10:20:55 +0100

    Redirection to Google login page.

--- src/main/java/rnd/mate00/oauth2client/config/SecurityConfig.java
+++ src/main/java/rnd/mate00/oauth2client/config/SecurityConfig.java
@@ -3,20 +3,18 @@ package rnd.mate00.oauth2client.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/allowed", "/a").permitAll()
-                .antMatchers("/restricted", "/r").authenticated();
+                .antMatchers("/restricted", "/r").authenticated()
+                .and().oauth2Login();
     }
 
     @Override
@@ -25,5 +23,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .ignoring()
             .antMatchers("/h2-console/**");
     }
-
 }
